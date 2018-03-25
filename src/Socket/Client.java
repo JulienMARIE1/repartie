5b964cc7c86ce7aac2f17bba5e8d5f2e604d6953
JulienMARIE1 <julienmarie83560@gmail.com>
2018-03-25 20:00:41 +0200

    exo1

--- src/Socket/Client.java
+++ src/Socket/Client.java
@@ -1,4 +1,4 @@
-package Socket;
+//package Socket;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,40 +7,74 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-
+import java.util.Scanner;
 public class Client {
 	private BufferedReader lecture;
 	private PrintWriter ecriture;
 	private Socket sock;
-	
+	private Socket maSocket;
+
 	public Client (){
 		try {
 			connexionServeur("127.0.0.1", 5000);
+			Thread e = new Thread(new ThreadEnvoi());
+			e.start();
+			Thread r = new Thread(new ThreadRecoit());
+			r.start();
+
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (Exception e){
+			e.printStackTrace();
 		}
 	}
+
+	public class ThreadEnvoi implements Runnable{
+	
+		@Override
+		public void run (){
+			while (true){
+			try {
+				ecriture = new PrintWriter(maSocket.getOutputStream());
+				Scanner sc = new Scanner(System.in);
+				System.out.println("Veuillez saisir un mot :");
+				String str = sc.nextLine(); 
+				ecriture.println(str);
+				ecriture.flush();
+			} catch (Exception e){
+				e.printStackTrace();
+			}
+
+			}
+		}
+	}
+	
+	public class ThreadRecoit implements Runnable{
+		@Override
+		public void run (){
+			while (true){
+				try{
+					InputStream monFlotBasNiveau = maSocket.getInputStream();
+					InputStreamReader monFlotDeCaractere = new InputStreamReader(monFlotBasNiveau);
+					lecture =  new BufferedReader(monFlotDeCaractere);
+					String monMessage = lecture.readLine();
+					System.out.println(monMessage);
+				} catch (Exception e){
+					e.printStackTrace();						
+				}
+
+			}
+			
+
+		}
+	
+	}
 	private void connexionServeur(String adresseIPServeur, int portServer) throws UnknownHostException, IOException{
-		Socket maSocket = new Socket (adresseIPServeur, portServer);
-		
-		InputStream monFlotBasNiveau = maSocket.getInputStream();
-		InputStreamReader monFlotDeCaractere = new InputStreamReader(monFlotBasNiveau);
-		this.lecture =  new BufferedReader(monFlotDeCaractere);
-		String monMessage = this.lecture.readLine();
-		System.out.println(monMessage);
-		this.lecture.close();
-		
-		
-		this.ecriture = new PrintWriter(maSocket.getOutputStream());
-		
-		this.ecriture.println("Burn Brain");
-		this.ecriture.flush();
-		this.ecriture.close();
-		
+		this.maSocket = new Socket (adresseIPServeur, portServer);	
 	}
 	
 	public static void main(String[] args) {
